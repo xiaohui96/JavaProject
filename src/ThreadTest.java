f1dac6623a43499bb37eee69f28c271b9e90aef6
xiaohui96 <xiaohui1295371450@163.com>
2019-10-09 19:47:54 +0800

    继承Thread类

---
+++ src/ThreadTest.java
@@ -0,0 +1,16 @@
+// 指定类继承Thread类
+public class ThreadTest extends Thread {
+    private int count = 10;
+    // 重写run()方法
+    public void run(){
+        while(true){
+            System.out.println(count+"");
+            if (--count == 0){
+                return;
+            }
+        }
+    }
+    public static void main(String[] args){
+        new ThreadTest().start();
+    }
+}
